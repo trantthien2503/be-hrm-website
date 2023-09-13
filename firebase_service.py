0701a98b293fd8483bf0143440a27c4bed876667
trantthien2503 <49374309+trantthien2503@users.noreply.github.com>
2023-09-13 14:58:46 +0700

    Update firebase_service.py

--- firebase_service.py
+++ firebase_service.py
@@ -17,22 +17,50 @@ class HRMCollection():
     def __init__(self, collection_name):
         self.collection = db.reference(collection_name)
 
-    def __getSnapshot(self):
-        return self.collection.get()
-
     def get_all_data(self):
-        snapshot = self.__getSnapshot()
-        if snapshot is None:
+        try:
+            snapshot = self.collection.get()
+            if snapshot is None:
+                return []
+            all_data = list(snapshot.values())
+            return all_data
+        except Exception as e:
+            print("Lỗi khi lấy dữ liệu:", str(e))
             return []
-        all_data = []
-        for key, value in snapshot.items():
-            data = value.copy()  # Tạo bản sao của value để tránh thay đổi trong dictionary gốc
-            all_data.append(data)
-        return all_data
 
-    def add_data(self, content):
-        new_data = self.collection.push()
-        new_id = new_data.key.lstrip("-")
-        content['_id'] = new_id
-        new_data.set(content)
-        return content
+    def add_data(self, data):
+        try:
+            new_data = self.collection.push()
+            new_id = new_data.key
+            data['_id'] = new_id
+            new_data.set(data)
+            return data
+        except Exception as e:
+            print("Lỗi khi thêm dữ liệu:", str(e))
+            return None
+
+    def update_data(self, data_id, new_data):
+        try:
+            data_ref = self.collection.child(data_id)
+            data_ref.set(new_data)
+        except Exception as e:
+            print("Lỗi khi cập nhật dữ liệu:", str(e))
+
+    def delete_data(self, data_id):
+        try:
+            data_ref = self.collection.child(data_id)
+            data_ref.delete()
+        except Exception as e:
+            print("Lỗi khi xóa dữ liệu:", str(e))
+
+    def search_data(self, field, value):
+        try:
+            query = self.collection.order_by_child(field).equal_to(value)
+            result = query.get()
+            if result is None:
+                return []
+            data = list(result.values())
+            return data
+        except Exception as e:
+            print("Lỗi khi tìm kiếm dữ liệu:", str(e))
+            return []
