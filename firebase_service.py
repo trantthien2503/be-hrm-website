26796e6e25505dfc3a463f43ecb7c0b107113cdd
trantthien2503 <49374309+trantthien2503@users.noreply.github.com>
2023-10-05 15:16:43 +0700

    update service

--- firebase_service.py
+++ firebase_service.py
@@ -1,8 +1,8 @@
 import firebase_admin
-from firebase_admin import credentials, db, auth
+from firebase_admin import credentials, db, auth,firestore
+
 import json
-import random
-import string
+
 with open('resful-api-38eda-firebase-adminsdk-6u38o-0f93a07228.json', 'r') as json_file:
     appsettings = json.load(json_file)
 
@@ -11,146 +11,145 @@ certificate = credentials.Certificate(appsettings)
 firebaseApp = firebase_admin.initialize_app(
     certificate, {"databaseURL": appsettings['databaseURL']})
 
+db = firestore.client()
 
-class HRMCollection():
+
+class FirestoreCollection():
 
     def __init__(self, collection_name):
-        self.collection = db.reference(collection_name)
+        self.collection = db.collection(collection_name)
 
     def get_all_data(self):
-        # Lấy toàn bộ dữ liệu từ collection
+        """Return all documents in collection"""
         try:
-            snapshot = self.collection.get()
-            if snapshot is None:
-                return []
-            all_data = list(snapshot.values())
-            return all_data
+            collections = self.collection.get()
+            collections_dict = []
+            
+            for doc in collections:
+                collections_dict.append(doc.to_dict())
+            
+            return collections_dict
         except Exception as e:
-            print("Lỗi khi lấy dữ liệu:", str(e))
-            return []
+            print("Error getting collections:", e)
+            return []    
 
-    def get_data_by_field(self, field_name, field_value):
-        # Lấy dữ liệu từ collection dựa trên trường và giá trị truyền vào
-        try:
-            query = self.collection.order_by_child(
-                field_name).equal_to(field_value)
-            snapshot = query.get()
-            if snapshot is None:
-                return []
-            data = list(snapshot.values())
-            return data
-        except Exception as e:
-            print("Lỗi khi lấy dữ liệu:", str(e))
-            return []
+
+    # def get_data_by_field(self, field_name, field_value):
+    #     # Lấy dữ liệu từ collection dựa trên trường và giá trị truyền vào
+    #     try:
+    #         query = self.collection.order_by_child(
+    #             field_name).equal_to(field_value)
+    #         snapshot = query.get()
+    #         if snapshot is None:
+    #             return []
+    #         data = list(snapshot.values())
+    #         return data
+    #     except Exception as e:
+    #         print("Lỗi khi lấy dữ liệu:", str(e))
+    #         return []
 
     def add_data(self, data):
-        # Thêm dữ liệu mới vào collection
         try:
-            new_data = self.collection.push()
-            new_id = new_data.key.lstrip("-")
-            data['_id'] = new_id
-            new_data.set(data)
-            return data, 200  # Trả về dữ liệu và mã trạng thái thành công
-        except Exception as e:
-            print("Lỗi khi thêm dữ liệu:", str(e))
-            return None, 500  # Trả về None và mã trạng thái lỗi
+            # Tạo document mới 
+            doc_ref = self.collection.document()  
+            doc_ref.set(data)
 
-    def update_data(self, data_id, new_data):
+            doc = doc_ref.get()
+            doc_data = doc.to_dict()
+
+            return doc_data
+
+        except Exception as e:
+            print("Error adding document: ", e)
+            return None
+
+    def update_data(self, doc_id, updates):
         # Cập nhật dữ liệu trong collection
         try:
-            # Tìm kiếm dữ liệu theo trường _id trong collection
-            query = self.collection.order_by_child(
-                '_id').equal_to(data_id).get()
+            # Lấy document theo id
+            doc = self.collection.document(doc_id)
+            
+            # Cập nhật dữ liệu
+            doc.update(updates)
 
-            # Kiểm tra xem có dữ liệu tồn tại hay không
-            if query:
-                # Lấy khóa của dữ liệu tồn tại
-                existing_key = list(query.keys())[0]
-
-                # Thực hiện cập nhật dữ liệu
-                self.collection.child(existing_key).update(new_data)
-                return None
-
-            else:
-                print("Không tìm thấy dữ liệu với _id =", data_id)
         except Exception as e:
-            print("Lỗi khi cập nhật dữ liệu:", str(e))
+            print("Error updating document: ", e)
 
-    def delete_data(self, data_id):
-        # Xóa dữ liệu khỏi collection
-        try:
-            print("data_id \n", data_id)
-            # Tìm kiếm dữ liệu theo trường _id trong collection
-            query = self.collection.order_by_child(
-                '_id').equal_to(data_id).get()
+    # def delete_data(self, data_id):
+    #     # Xóa dữ liệu khỏi collection
+    #     try:
+    #         print("data_id \n", data_id)
+    #         # Tìm kiếm dữ liệu theo trường _id trong collection
+    #         query = self.collection.order_by_child(
+    #             '_id').equal_to(data_id).get()
 
-            # Kiểm tra xem có dữ liệu tồn tại hay không
-            if query:
-                # Lấy khóa của dữ liệu tồn tại
-                existing_key = list(query.keys())[0]
+    #         # Kiểm tra xem có dữ liệu tồn tại hay không
+    #         if query:
+    #             # Lấy khóa của dữ liệu tồn tại
+    #             existing_key = list(query.keys())[0]
 
-                # Thực hiện xóa dữ liệu
-                self.collection.child(existing_key).delete()
+    #             # Thực hiện xóa dữ liệu
+    #             self.collection.child(existing_key).delete()
 
-                return None
-            else:
-                print("Không tìm thấy dữ liệu với _id =", data_id)
-        except Exception as e:
-            print("Lỗi khi xóa dữ liệu:", str(e))
+    #             return None
+    #         else:
+    #             print("Không tìm thấy dữ liệu với _id =", data_id)
+    #     except Exception as e:
+    #         print("Lỗi khi xóa dữ liệu:", str(e))
 
-    def search_data(self, field, value):
-        # Tìm kiếm dữ liệu trong collection dựa trên trường và giá trị
-        try:
-            query = self.collection.order_by_child(field).equal_to(value)
-            result = query.get()
-            if result is None:
-                return []
-            data = list(result.values())
-            return data
-        except Exception as e:
-            print("Lỗi khi tìm kiếm dữ liệu:", str(e))
-            return []
+    # def search_data(self, field, value):
+    #     # Tìm kiếm dữ liệu trong collection dựa trên trường và giá trị
+    #     try:
+    #         query = self.collection.order_by_child(field).equal_to(value)
+    #         result = query.get()
+    #         if result is None:
+    #             return []
+    #         data = list(result.values())
+    #         return data
+    #     except Exception as e:
+    #         print("Lỗi khi tìm kiếm dữ liệu:", str(e))
+    #         return []
 
-    def create_user(self, email, password):
-        # Tạo người dùng mới với email và mật khẩu.
-        # Trả về thông tin người dùng được tạo thành công hoặc None nếu có lỗi.
-        try:
-            user = auth.create_user(
-                email=email,
-                password=password
-            )
-            return user
-        except Exception as e:
-            print("Lỗi khi tạo người dùng:", str(e))
-            return None
+    # def create_user(self, email, password):
+    #     # Tạo người dùng mới với email và mật khẩu.
+    #     # Trả về thông tin người dùng được tạo thành công hoặc None nếu có lỗi.
+    #     try:
+    #         user = auth.create_user(
+    #             email=email,
+    #             password=password
+    #         )
+    #         return user
+    #     except Exception as e:
+    #         print("Lỗi khi tạo người dùng:", str(e))
+    #         return None
 
-    def delete_user(self, user_id):
-        # Xóa người dùng dựa trên user_id.
-        try:
-            auth.delete_user(user_id)
-        except Exception as e:
-            print("Lỗi khi xóa người dùng:", str(e))
+    # def delete_user(self, user_id):
+    #     # Xóa người dùng dựa trên user_id.
+    #     try:
+    #         auth.delete_user(user_id)
+    #     except Exception as e:
+    #         print("Lỗi khi xóa người dùng:", str(e))
 
-    def get_user(self, user_id):
-        # Lấy thông tin người dùng dựa trên user_id.
-        # Trả về thông tin người dùng hoặc None nếu không tìm thấy người dùng.
-        try:
-            user = auth.get_user(user_id)
-            return user
-        except Exception as e:
-            print("Lỗi khi lấy thông tin người dùng:", str(e))
-            return None
+    # def get_user(self, user_id):
+    #     # Lấy thông tin người dùng dựa trên user_id.
+    #     # Trả về thông tin người dùng hoặc None nếu không tìm thấy người dùng.
+    #     try:
+    #         user = auth.get_user(user_id)
+    #         return user
+    #     except Exception as e:
+    #         print("Lỗi khi lấy thông tin người dùng:", str(e))
+    #         return None
 
-    def update_user(self, user_id, display_name=None, password=None):
-        # Cập nhật thông tin người dùng dựa trên user_id.
-        # Trả về thông tin người dùng sau khi cập nhật hoặc None nếu có lỗi.
-        try:
-            user = auth.update_user(
-                user_id,
-                display_name=display_name,
-                password=password
-            )
-            return user
-        except Exception as e:
-            print("Lỗi khi cập nhật người dùng:", str(e))
-            return None
+    # def update_user(self, user_id, display_name=None, password=None):
+    #     # Cập nhật thông tin người dùng dựa trên user_id.
+    #     # Trả về thông tin người dùng sau khi cập nhật hoặc None nếu có lỗi.
+    #     try:
+    #         user = auth.update_user(
+    #             user_id,
+    #             display_name=display_name,
+    #             password=password
+    #         )
+    #         return user
+    #     except Exception as e:
+    #         print("Lỗi khi cập nhật người dùng:", str(e))
+    #         return None
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from firebase_service import HRMCollection
+from firebase_service import FirestoreCollection
 from datetime import datetime
 
 # Tạo blueprint với tên là "main"
@@ -9,20 +9,20 @@ main_bp = Blueprint('main', __name__)
 # Api lấy danh toàn bộ danh sách actors
 @main_bp.route('/api/get-actors', methods=['GET'])
 def getActor():
-    hrm = HRMCollection("actors")
-    data = hrm.get_all_data()
+    fire = FirestoreCollection("actors")
+    data = fire.get_all_data()
     return jsonify(data)
 
 
-# APi lấy actor theo id
-@main_bp.route('/api/get-actor', methods=['POST'])
-def getActorById():
-    req = request.get_json()
-    idActor = req.get('_id')
-    hrm = HRMCollection("actors")
-    data = hrm.get_data_by_field('_id', idActor)[0]
-    # dữ liệu phương thức get_data_by_field sẽ trả về dạng mảng
-    return jsonify(data)
+# # APi lấy actor theo id
+# @main_bp.route('/api/get-actor', methods=['POST'])
+# def getActorById():
+#     req = request.get_json()
+#     idActor = req.get('_id')
+#     hrm = HRMCollection("actors")
+#     data = hrm.get_data_by_field('_id', idActor)[0]
+#     # dữ liệu phương thức get_data_by_field sẽ trả về dạng mảng
+#     return jsonify(data)
 
 
 # Api api thêm 1 actor
@@ -31,49 +31,48 @@ def addActor():
    # Thực hiện lấy toàn bộ dữ liệu truyền từ client theo dạng post
     req = request.get_json()
     data = {
-        'first_name': req.get('first_name'),
-        'last_name': req.get('last_name'),
-        'last_update': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        'firstname': req.get('firstname'),
+        'lastname': req.get('lastname'),
     }
-    hrm = HRMCollection("actors")
-    new_id = hrm.add_data(data)
-    return jsonify(new_id)
+    fire = FirestoreCollection("actors")
+    newData = fire.add_data(data)
+    return jsonify(newData)
 
 
-# Api api cập 1 actor theo trường _id
-@main_bp.route('/api/update-actor', methods=['POST'])
-def updateActorById():
-   # Thực hiện lấy toàn bộ dữ liệu truyền từ client theo dạng post
-    req = request.get_json()
-    idActor = req.get('_id')
-    dataUpdate = {
-        'first_name': req.get('first_name'),
-        'last_name': req.get('last_name'),
-        'last_update': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    }
-    hrm = HRMCollection("actors")
-    data = hrm.update_data(idActor, dataUpdate)
-    # Kiểm tra dữ liệu trả về là None thì thành công
-    if (data == None):
-        return jsonify(
-            {'message': 'Cập nhật thành công !', 'result': True})
-    else:
-        return jsonify(
-            {'message': 'Cập nhật không thành công !', 'result': False})
+# # Api api cập 1 actor theo trường _id
+# @main_bp.route('/api/update-actor', methods=['POST'])
+# def updateActorById():
+#    # Thực hiện lấy toàn bộ dữ liệu truyền từ client theo dạng post
+#     req = request.get_json()
+#     idActor = req.get('_id')
+#     dataUpdate = {
+#         'firstname': req.get('firstname'),
+#         'lastname': req.get('lastname'),
+#         'last_update': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#     }
+#     hrm = HRMCollection("actors")
+#     data = hrm.update_data(idActor, dataUpdate)
+#     # Kiểm tra dữ liệu trả về là None thì thành công
+#     if (data == None):
+#         return jsonify(
+#             {'message': 'Cập nhật thành công !', 'result': True})
+#     else:
+#         return jsonify(
+#             {'message': 'Cập nhật không thành công !', 'result': False})
 
 
-# Api api xóa 1 actor
-@main_bp.route('/api/delete-actor', methods=['POST'])
-def deleteActorById():
-   # Thực hiện lấy toàn bộ dữ liệu truyền từ client theo dạng post
-    req = request.get_json()
-    idActor = req.get('_id')
-    hrm = HRMCollection("actors")
-    data = hrm.delete_data(idActor)
-    # Kiểm tra dữ liệu trả về là None thì thành công
-    if (data == None):
-        return jsonify(
-            {'message': 'Xóa thành công !', 'result': True})
-    else:
-        return jsonify(
-            {'message': 'Xóa không thành công !', 'result': False})
+# # Api api xóa 1 actor
+# @main_bp.route('/api/delete-actor', methods=['POST'])
+# def deleteActorById():
+#    # Thực hiện lấy toàn bộ dữ liệu truyền từ client theo dạng post
+#     req = request.get_json()
+#     idActor = req.get('_id')
+#     hrm = HRMCollection("actors")
+#     data = hrm.delete_data(idActor)
+#     # Kiểm tra dữ liệu trả về là None thì thành công
+#     if (data == None):
+#         return jsonify(
+#             {'message': 'Xóa thành công !', 'result': True})
+#     else:
+#         return jsonify(
+#             {'message': 'Xóa không thành công !', 'result': False})
